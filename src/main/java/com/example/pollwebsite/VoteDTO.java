a7961a66868a3a7c9daa8eb611aec23684b06519
The-Multitud-e <chris_dooley123@hotmail.co.uk>
2025-02-14 15:23:38 +0000

    add field to vote DTO

--- src/main/java/com/example/pollwebsite/VoteDTO.java
+++ src/main/java/com/example/pollwebsite/VoteDTO.java
@@ -2,17 +2,30 @@ package com.example.pollwebsite;
 
 import java.time.ZonedDateTime;
 import java.util.Objects;
+import java.util.UUID;
 
 public class VoteDTO {
 
+    private UUID uuid;
     private String pollName;
     private String option;
+    private int voteNumber;
     private ZonedDateTime timestamp;
 
-    public VoteDTO(String pollName, String option, ZonedDateTime timestamp) {
+    public VoteDTO(UUID uuid, String pollName, String option, int voteNumber, ZonedDateTime timestamp) {
+        this.uuid = uuid;
         this.pollName = pollName;
         this.option = option;
+        this.voteNumber = voteNumber;
         this.timestamp = timestamp;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
     }
 
     public String getPollName() {
@@ -31,6 +44,14 @@ public class VoteDTO {
         this.option = option;
     }
 
+    public int getVoteNumber() {
+        return voteNumber;
+    }
+
+    public void setVoteNumber(int voteNumber) {
+        this.voteNumber = voteNumber;
+    }
+
     public ZonedDateTime getTimestamp() {
         return timestamp;
     }
@@ -43,20 +64,23 @@ public class VoteDTO {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         VoteDTO voteDTO = (VoteDTO) o;
-        return Objects.equals(pollName, voteDTO.pollName) && Objects.equals(option, voteDTO.option) && Objects.equals(timestamp, voteDTO.timestamp);
+        return voteNumber == voteDTO.voteNumber && Objects.equals(uuid, voteDTO.uuid) && Objects.equals(pollName, voteDTO.pollName) && Objects.equals(option, voteDTO.option) && Objects.equals(timestamp, voteDTO.timestamp);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(pollName, option, timestamp);
+        return Objects.hash(uuid, pollName, option, voteNumber, timestamp);
     }
 
     @Override
     public String toString() {
         return "VoteDTO{" +
-                "pollName='" + pollName + '\'' +
+                "uuid=" + uuid +
+                ", pollName='" + pollName + '\'' +
                 ", option='" + option + '\'' +
+                ", voteNumber=" + voteNumber +
                 ", timestamp=" + timestamp +
                 '}';
     }
 }
+
