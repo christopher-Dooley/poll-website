64185b38bca4503cb5259212dc641aad85903ffe
The-Multitud-e <chris_dooley123@hotmail.co.uk>
2025-02-14 17:38:31 +0000

    poll controller

---
+++ src/main/java/com/example/pollwebsite/PollController.java
@@ -0,0 +1,80 @@
+package com.example.pollwebsite;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.UUID;
+
+@RestController
+public class PollController {
+
+    private final PollService pollService;
+
+    @Autowired
+    public PollController(PollService pollService) {
+        this.pollService = pollService;
+    }
+
+    @GetMapping("/poll/current")
+    public PollDTO getCurrentPoll() {
+        return pollService.getCurrentPoll();
+    }
+
+    @GetMapping("/poll/name/{name}")
+    public PollDTO getPollByName(@PathVariable String name) {
+        return pollService.getPollByName(name);
+    }
+
+    @GetMapping("/poll/uuid/{uuid}")
+    public PollDTO getPollByUUID(@PathVariable UUID uuid) {
+        return pollService.getPollByUUID(uuid);
+    }
+
+    @PostMapping("/poll/save")
+    public ResponseEntity<PollDTO> savePoll(@RequestBody PollDTO pollDTO, HttpServletRequest request) {
+        try {
+            String requestUri = request.getRequestURI();
+            PollDTO savedPoll = pollService.savePoll(pollDTO);
+            URI location = getNewLocationUri(requestUri, savedPoll.getUuid());
+            return ResponseEntity.created(location).body(savedPoll);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    private URI getNewLocationUri(String requestUri, UUID uuid) {
+        return URI.create(
+                requestUri.substring(0, requestUri.length() -4)
+                        .concat("uuid/")
+                        .concat(uuid.toString())
+        );
+    }
+
+    @GetMapping("/poll/results")
+    public PollResultDTO getResultsForCurrentPoll() {
+        return pollService.getResultsForCurrentPoll();
+    }
+
+    @GetMapping("/poll/name/{name}/results")
+    public PollResultDTO getResultsForPollByName(@PathVariable String name) {
+        return pollService.getResultsForPoll(name);
+    }
+
+    @GetMapping("/poll/uuid/{uuid}/results")
+    public PollResultDTO getResultsForPollByUUID(@PathVariable UUID uuid) {
+        return pollService.getResultsForPollByUUID(uuid);
+    }
+
+    @PostMapping("/poll/vote")
+    public VoteDTO saveVote(@RequestBody VoteDTO voteDTO) {
+        return pollService.saveVote(voteDTO);
+    }
+
+    @ExceptionHandler(PollNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Poll not found")
+    public void handlePollNotFoundException() {}
+}
