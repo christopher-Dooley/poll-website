74441a86906a339928477e856ac49bb5a51fef6c
The-Multitud-e <chris_dooley123@hotmail.co.uk>
2025-02-14 12:39:53 +0000

    poll repository

---
+++ src/main/java/com/example/pollwebsite/PollRepository.java
@@ -0,0 +1,13 @@
+package com.example.pollwebsite;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface PollRepository extends CrudRepository<PollEntity, UUID> {
+    PollEntity save(PollEntity pollEntity);
+
+    PollEntity findByName(String name);
+
+    PollEntity findByUUID(UUID uuid);
+}
